bc84b451afcf6d27010bfda7c23a05961c784bab
Max <zeetreex@gmail.com>
2023-01-04 20:02:22 +0200

    added all needed styles and material ui components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/ui/header/header.component';
-import { FooterComponent } from './components/ui/footer/footer.component';
-import { ProductsComponent } from './components/products/products.component';
-import { BasketComponent } from './components/basket/basket.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { BaseComponent } from './components/base/base.component';
-import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/ui/header/header.component';
+import {FooterComponent} from './components/ui/footer/footer.component';
+import {ProductsComponent} from './components/products/products.component';
+import {BasketComponent} from './components/basket/basket.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {BaseComponent} from './components/base/base.component';
+import {DialogBoxComponent} from './components/dialog-box/dialog-box.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -26,9 +30,14 @@ import { DialogBoxComponent } from './components/dialog-box/dialog-box.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
