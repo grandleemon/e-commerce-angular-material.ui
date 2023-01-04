d0e938e2f6b7bfdb7db37a03305d0243777b8651
Max <zeetreex@gmail.com>
2023-01-04 21:40:34 +0200

    configured json server

---
+++ src/app/models/products.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  id: number
+  title: string
+  price: number
+  image?: string
+  configure: IProductConfig
+}
+
+export interface IProductConfig {
+  chip: string
+  SSD: string
+  memory: string
+  display: string
+}
